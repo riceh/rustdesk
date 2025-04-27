6404ac61e8d7c1869e694969b159f0a41a6a5294
riceh <riceh@qq.com>
2025-04-28 01:17:15 +0800

    修改子模块仓库地址,修改默认API服务器地址

--- flutter/lib/desktop/pages/connection_page.dart
+++ flutter/lib/desktop/pages/connection_page.dart
@@ -86,24 +86,24 @@ class _OnlineStatusWidgetState extends State<OnlineStatusWidget> {
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Text(', ', style: TextStyle(fontSize: em)),
-                Flexible(
-                  child: InkWell(
-                    onTap: onUsePublicServerGuide,
-                    child: Row(
-                      children: [
-                        Flexible(
-                          child: Text(
-                            translate('setup_server_tip'),
-                            style: TextStyle(
-                                decoration: TextDecoration.underline,
-                                fontSize: em),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                )
+                // Text(', ', style: TextStyle(fontSize: em)),
+                // Flexible(
+                //   child: InkWell(
+                //     onTap: onUsePublicServerGuide,
+                //     child: Row(
+                //       children: [
+                //         Flexible(
+                //           child: Text(
+                //             translate('setup_server_tip'),
+                //             style: TextStyle(
+                //                 decoration: TextDecoration.underline,
+                //                 fontSize: em),
+                //           ),
+                //         ),
+                //       ],
+                //     ),
+                //   ),
+                // )
               ],
             ),
           ),
